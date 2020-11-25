a9966794ce6ad985ef5650de9324036dc26389f2
Dave McMahon <davidmcmhn@gmail.com>
2020-11-25 20:21:03 +0000

    Fixed syntax error missing {

--- instance-within-vpc/instance.tf
+++ instance-within-vpc/instance.tf
@@ -6,19 +6,21 @@ resource "aws_instance" "dmc-ec2-example" {
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
   key_name = aws_key_pair.terrakeys.key_name
   user_data = data.template_cloudinit_config.cloudinit-dmc.rendered
+}
 
-  resource "aws_ebs_volume" "ebs-dmc-volume" {
-    availability_zone = "eu-west-1a"
-    size = "20"
-    type = "gp2"
-    tags = {
-      Name = "extra data storage"
-    }
+resource "aws_ebs_volume" "ebs-dmc-volume" {
+  availability_zone = "eu-west-1a"
+  size = "20"
+  type = "gp2"
+  tags = {
+    Name = "extra data storage"
   }
+}
 
-  resource "aws_volume_attachment" "ebs-dmc-volume-attachment" {
-    device_name = var.DEVICE_NAME
-    volume_id = aws_ebs_volume.ebs-dmc-volume.id
-    instance_id = aws_instance.dmc-ec2-example.id
-    skip_destroy = true
-  }
+resource "aws_volume_attachment" "ebs-dmc-volume-attachment" {
+  device_name = var.DEVICE_NAME
+  volume_id = aws_ebs_volume.ebs-dmc-volume.id
+  instance_id = aws_instance.dmc-ec2-example.id
+  skip_destroy = true
+}
+  
