8f3fc28ed7e7070667f2691c1934e3ee7ee17dbb
Dave McMahon <davidmcmhn@gmail.com>
2020-11-25 20:06:23 +0000

    added ebs storage volume and its attachment config

--- instance-within-vpc/instance.tf
+++ instance-within-vpc/instance.tf
@@ -4,5 +4,20 @@ resource "aws_instance" "dmc-ec2-example" {
 
   subnet_id = aws_subnet.dmc-public-1.id
   vpc_security_group_ids = [aws_security_group.allow-ssh.id]
-  key_name = aws_key_pair.terrakeys.key_name
-}
+  key_name = 
+
+  resource "aws_ebs_volume" "ebs-dmc-volume" {
+    availability_zone = "eu-west-1a"
+    size = "20"
+    type = "gp2"
+    tags = {
+      Name = "extra data storage"
+    }
+  }
+
+  resource "aws_volume_attachment" "ebs-dmc-volume-attachment" {
+    device_name = var.DEVICE_NAME
+    volume_id = aws_ebs_volume.ebs-dmc-volume.id
+    instance_id = aws_instance.dmc-ec2-example.id
+    skip_destroy = true
+  }
